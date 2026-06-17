e15dfa61ec53af2cc47b62f67ac51391a6f38380
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-17 12:38:50 +0200

    add dom

--- esercizio-2/README.md
+++ esercizio-2/README.md
@@ -1,5 +1,4 @@
 # Esercizio
+crea un input e un paragrafo. Mentre l'utente digita nell'input, il testo del paragrafo si aggiorna in tempo reale
 
-
-## Ragionamento
 
--- esercizio-2/assets/main.js
+++ esercizio-2/assets/main.js
@@ -1,1 +1,11 @@
 console.log("js caricato");
+
+
+const inputField = document.getElementById('mirror-input');
+const outputText = document.getElementById('mirror-text');
+
+
+inputField.addEventListener('input', () => {
+   outputText.innerText = inputField.value;
+});
+
