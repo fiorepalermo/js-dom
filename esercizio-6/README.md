7d1560bb2d5022be1e3975de92e59bb00cfe68fb
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-17 13:16:25 +0200

    add dom

--- esercizio-6/README.md
+++ esercizio-6/README.md
@@ -1,5 +1,5 @@
 # Esercizio
-
+dato un oggetto utente (nome, ruolo, img) e una struttura HTML vuota, inserisci i dati dell'oggetto nei rispettivi elementi della pagina
 
 ## Ragionamento
 
--- esercizio-6/assets/main.js
+++ esercizio-6/assets/main.js
@@ -1,1 +1,12 @@
 console.log("js caricato");
+
+
+const user = {
+    nome: 'Elisabetta',
+    ruolo: 'Web Developer',
+    img: 'https://picsum.photos/100'
+};
+
+document.getElementById('user-name').innerText = user.nome;
+document.getElementById('user-role').innerText = user.ruolo;
+document.getElementById('user-img').src = user.img; 
