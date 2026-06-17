57fb0e05c01b753fd288f40c3fa3c648bcfd2213
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-17 13:03:18 +0200

    add dom

--- esercizio-3/README.md
+++ esercizio-3/README.md
@@ -1,5 +1,5 @@
 # Esercizio
-
+crea un input e un pulsante disabilitato. Abilita il pulsante solo quando l'input contiene almeno 3 caratteri
 
 ## Ragionamento
 
--- esercizio-3/assets/main.js
+++ esercizio-3/assets/main.js
@@ -1,1 +1,8 @@
 console.log("js caricato");
+
+const usernameInput = document.getElementById('username');
+const submitBtn = document.getElementById('submit-btn');
+
+usernameInput.addEventListener('input', () => {
+    submitBtn.disabled = usernameInput.value.length < 3;
+});
