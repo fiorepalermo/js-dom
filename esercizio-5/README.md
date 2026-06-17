a433393e0928402979c430902a2267534cdf548e
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-17 13:13:57 +0200

    add dom

--- esercizio-5/README.md
+++ esercizio-5/README.md
@@ -1,5 +1,5 @@
 # Esercizio
-
+dato un prezzo fisso e un input per la quantità, aggiorna in tempo reale un paragrafo che mostra il totale
 
 ## Ragionamento
 
--- esercizio-5/assets/main.js
+++ esercizio-5/assets/main.js
@@ -1,1 +1,13 @@
 console.log("js caricato");
+
+const priceSpan = document.getElementById('unit-price');
+const quantityInput = document.getElementById('quantity');
+const totalSpan = document.getElementById('total-price');
+
+const unitPrice = Number(priceSpan.innerText);
+
+quantityInput.addEventListener('input', () => {
+    const qty = Number(quantityInput.value);
+    const total = unitPrice * qty;
+    totalSpan.innerText = total; 
+});
