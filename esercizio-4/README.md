93f533a990318b173b7173c0e1c230f93c2ac28f
Fiore Palermo <fiorepalermo@gmail.com>
2026-06-17 13:09:37 +0200

    add dom

--- esercizio-4/README.md
+++ esercizio-4/README.md
@@ -1,5 +1,7 @@
 # Esercizio
 
+con due input password, mostra un messaggio di errore rosso se i valori non coincidono mentre l'utente digita nel secondo campo
 
-## Ragionamento
+
+
 
--- esercizio-4/assets/main.js
+++ esercizio-4/assets/main.js
@@ -1,1 +1,13 @@
 console.log("js caricato");
+
+const p1 = document.getElementById('p1');
+const p2 = document.getElementById('p2');
+const errorMsg = document.getElementById('error-message');
+
+p2.addEventListener('input', () => {
+    if (p1.value !== p2.value) {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+    }
+});
